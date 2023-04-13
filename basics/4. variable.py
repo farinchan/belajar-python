ec4e3a44f46628d332062ca91ce3687959facfe5
fajri <fajririnaldichan@gmail.com>
2023-04-13 12:39:05 +0700

    variabel

---
+++ basics/4. variable.py
@@ -0,0 +1,24 @@
+#variable
+#variabel adalah tempat menyimpan data
+
+a = 10
+"""
+ket: 
+    a -> variable/ indetifier
+    = -> asignment
+    10 -> value
+"""
+print( "nilai a = ", a)
+#variable dapat dipanggil berulang
+print("nilai a lagi = ", a)
+
+"""
+Aturan penamaan variabel : 
+    Nama variabel hanya boleh terdiri dari huruf (a-z, A-Z), angka (0-9), dan underscore (_).
+    Nama variabel harus dimulai dengan huruf atau underscore (_), dan tidak boleh dimulai dengan angka.
+    Nama variabel harus bersifat case-sensitive, artinya huruf besar dan huruf kecil dianggap berbeda.
+    Nama variabel tidak boleh sama dengan kata kunci (keyword) yang sudah dikenal oleh Python, seperti "if", "else", "while", dan sebagainya.
+"""
+
+b = a
+print("Nilai b = ", b)
