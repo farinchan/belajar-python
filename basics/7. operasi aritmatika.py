2c23c9431c4085ea70f9070f76b0fe9f463b5f93
fajri <fajririnaldichan@gmail.com>
2023-04-13 18:16:12 +0700

    operasi aritmatika

---
+++ basics/7. operasi aritmatika.py
@@ -0,0 +1,33 @@
+# operasi aritmatika
+
+a = 10
+b = 5
+
+# operasi penjulahan
+hasil = a + b
+print(a, " + ", b, " = ",  hasil)
+
+# operasi pengurangan
+hasil = a - b
+print(a, " - ", b, " = ",  hasil)
+
+# operasi perkalian
+hasil = a * b
+print(a, " * ", b, " = ",  hasil)
+
+# operasi pembagian
+# pada python hasil bagi otomatis menjadi tipe data float
+hasil = a / b
+print(a, " / ", b, " = ",  hasil)
+
+# operasi modulus - sisa pembagian
+hasil = a % b
+print(a, " % ", b, " = ",  hasil)
+
+# operasi floor division - dibulatkan kebawah
+hasil = a // b
+print(a, " // ", b, " = ",  hasil)
+
+# operasi eksponen(pangkat)
+hasil = a ** b
+print(a, " ** ", b, " = ",  hasil)
