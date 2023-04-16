2f99560d24a1450cb294eac6d872ac69fb3e83f3
fajri <fajririnaldichan@gmail.com>
2023-04-16 18:15:20 +0700

    Manipulasi String

--- basics/12. string manipulation.py
+++ basics/12. string manipulation.py
@@ -37,3 +37,52 @@ print("ascii ocde dari char ini adalah" + str(ascii_code))
 data = "halo nama saya fajri rinaldi chan"
 jumlah = data.count("a")
 print("jumlah 'a' dalam data = ", str(jumlah))
+
+# 8. Mengganti string menjadi UpperCase
+halo = "Halo Dek"
+print("String Normal = ", halo)
+halo = halo.upper()
+print("String UpperCase = ", halo)
+
+# 8. Mengganti String Menjadi LowerCase
+halo = "Halo DekkZ, Mau SAma OMMZZ Gak?"
+print("String Normal = ", halo)
+halo = halo.lower()
+print("String UpperCase = ", halo)
+
+# 9 . Pengecekan lower/upper dll, x.method dalam string
+halo = "halo"
+halo_apakah_lower = halo.islower()  # hasilnya Boolean
+print("halo = " + str(halo_apakah_lower))
+halo_apakah_upper = halo.isupper()  # hasilnya Boolean
+print("halo = " + str(halo_apakah_upper))
+"""
+    isalpha() -> untuk ngecek semua huruf
+    isnum() -> pengecekan huruf dan angka
+    isdecimal() -> pengecekan angka saja
+    isspace() -> penegcekan spasi, tab, newline \n
+    istitle() -> pengecekan apakah semu kata dimulai dari huruf besar
+"""
+# 10. ngecek komponen startswith() dan endswith()
+halo = "halo dek".startswith("halo")
+print(halo)
+halo = "halo dek".endswith("halo")
+print(halo)
+
+# 11. penggabungan Component join() dan memisahkan komponen split()
+pisah = ["aku", "sayang", "kamu"]
+print(pisah)
+gabung = " ".join(pisah)
+print(gabung)
+print(gabung.split(" "))
+
+# 12. alokasi karakter rjust(), ljust(), center()
+kanan = "kanan".rjust(10)
+print("'" + kanan + "'")
+
+kiri = "kiri".ljust(10)
+print("'" + kiri + "'")
+
+tengah = "tengah".center(10)
+print("'" + tengah + "'")
+
