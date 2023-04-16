ccaaaf9c8b2ac8b7a5436b6e9e4fb80ad4c1194c
fajri <fajririnaldichan@gmail.com>
2023-04-16 12:37:29 +0700

    operator assignment

---
+++ basics/10. operator assignment.py
@@ -0,0 +1,27 @@
+# operator assignment ( = )
+
+a = 3  # adalah assignment
+print("Nilai a = ", a)
+
+a += 5  # adalah assignment -> a = a + 5
+print("Nilai a = ", a)
+
+a -= 2  # adalah assignment -> a = a + 2
+print("Nilai a = ", a)
+
+a *= 4  # adalah assignment -> a = a * 4
+print("Nilai a = ", a)
+
+a /= 2  # adalah assignment -> a = a / 2
+print("Nilai a = ", a)
+
+a **= 2  # adalah assignment -> a = a / 2
+print("Nilai a = ", a)
+
+a //= 2  # adalah assignment -> a = a / 2
+print("Nilai a = ", a)
+
+a %= 2  # adalah assignment -> a = a / 2
+print("Nilai a = ", a)
+
+
